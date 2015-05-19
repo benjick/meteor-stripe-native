3a7a27963f8390e500163a363c1bc8c7fbdffdba
Max Malm <benjick@dumfan.net>
2015-05-19 15:54:38 +0200

    added git url

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Implementing the Stripe API as a Meteor package without the Node module',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/benjick/meteor-stripe-native.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
