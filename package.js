fa60602ee3fb2c69cf80ded03ac6bff1fefcfa55
Max Malm <benjick@dumfan.net>
2015-05-20 22:33:08 +0200

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'benjick:stripe-native',
-  version: '0.2.1',
+  version: '0.3.0',
   summary: 'Implementing the Stripe API as a Meteor package without the Node module',
   git: 'https://github.com/benjick/meteor-stripe-native.git',
   documentation: 'README.md'
