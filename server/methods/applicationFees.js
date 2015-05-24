15d182d9ed68e9af8b413e22978d989b58ec4804
Max Malm <benjick@dumfan.net>
2015-05-24 11:51:27 +0200

    added method application fees

--- server/methods/applicationFees.js
+++ server/methods/applicationFees.js
@@ -1,1 +1,18 @@
 Stripe.applicationFees = {};
+
+Stripe.applicationFees.retrieve = function(id) {
+	var options = {
+		auth: Stripe.secretKey
+	}
+
+	return HTTP.call('GET', 'https://api.stripe.com/v1/application_fees/' + id, options);
+}
+
+Stripe.applicationFees.list = function(object) {
+	var options = {
+		auth: Stripe.secretKey,
+		params: object
+	}
+
+	return HTTP.call('GET', 'https://api.stripe.com/v1/application_fees', options);
+}
